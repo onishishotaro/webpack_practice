1758d5f5c2e63d639cf80c06ec54ce70603d2e78
onishishotaro <shoukiti.chan@gmail.com>
2020-10-25 23:47:34 +0900

    ES6, etc..

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,65 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
 const path = require("path");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 module.exports = {
+  devtool: "source-map",
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: "development",
+  // エントリーポイントの設定
   entry: "./src/js/main.js",
+  // 出力の設定
   output: {
+    // 出力先のパス（絶対パスを指定する必要がある）
     path: path.resolve(__dirname, "./dist"),
+    // 出力するファイル名
     filename: "js/main.js",
   },
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.vue/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "vue-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
           },
         ],
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: "file-loader",
@@ -30,6 +67,9 @@ module.exports = {
               esModule: false,
               name: "img/[name].[ext]",
             },
+          },
+          {
+            loader: "image-webpack-loader",
           },
         ],
       },
@@ -50,6 +90,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: "./css/main.css",
     }),
@@ -60,6 +101,10 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/templates/access.pug",
       filename: "access.html",
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/templates/members/taro.pug",
+      filename: "members/taro.html",
     }),
     new CleanWebpackPlugin(),
   ],
